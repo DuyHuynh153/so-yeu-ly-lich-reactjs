56e7cd28060650de9077e9c962b6ef7bcbd40323
Duy Huynh <huynhlamduylcd@gmail.com>
2024-04-24 18:17:40 +0700

    adjust  main form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
   const [myData, setMyData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [tinh, setTinh] = useState("Chọn tỉnh");
-  const abortController = new AbortController();
+  const [tinh, setTinh] = useState("");
+  const [tinhID, setTinhID] = useState("");
+
+  const location = useLocation();
+
+  const [formData, setFormData] = useState(null);
+
+  // const { data } = location.state;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,7 +21,7 @@ const Home = () => {
         const data = await response.json();
         setMyData(data.data);
         setLoading(false);
-        console.log("your data: ", data.data);
+        console.log("your data 3: ", data.data[0].data2[0].data3);
       } catch (err) {
         if (err.code === "AbortError") {
           console.log("AbortErro");
@@ -28,20 +35,98 @@ const Home = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (location.state && location.state.data) {
+      setFormData(location.state.data);
+      console.log("Form data received:", location.state.data);
+    }
+  }, [location.state]);
+
+  const handleChangeTinh = (e) => {
+    const select_tinh_id = e.target.value;
+    const select_tinh = myData.find((data) => data.id === select_tinh_id);
+    if (select_tinh) {
+      setTinh(select_tinh.full_name);
+      setTinhID(select_tinh_id - 1);
+      console.log("tinh id", select_tinh_id);
+    }
+  };
+
   return (
     <div>
-      <h1>this is home page</h1>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <select value={tinh} onChange={(e) => setTinh(e.target.value)}>
-          {myData.map((data, index) => (
-            <option key={index} value={data.id}>
-              {data.full_name}
-            </option>
-          ))}
-        </select>
-      )}
+      <div>
+        <h1>this is home page</h1>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <select onChange={handleChangeTinh}>
+            {myData.map((data, index) => (
+              <option key={index} value={data.id}>
+                {data.full_name}
+              </option>
+            ))}
+          </select>
+        )}
+
+        {tinh ? <p>{tinh}</p> : <p>Chọn tỉnh</p>}
+
+        {myData.length > 0 &&
+        tinhID !== "" &&
+        myData[parseInt(tinhID)].data2 ? (
+          <select>
+            {myData[parseInt(tinhID)].data2.map((data, index) => (
+              <option key={index} value={data.id}>
+                {data.full_name}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <p>No data yet</p>
+        )}
+      </div>
+      <div>
+        <h1>This is the home page</h1>
+        {formData && (
+          <div>
+            <h2>Thông Tin Cá Nhân</h2>
+            {formData.selfImagePictureUrl && (
+              <img
+                src={formData.selfImagePictureUrl}
+                alt="Self"
+                style={{ maxWidth: "200px" }}
+              />
+            )}
+            <p>
+              <strong>Họ:</strong> {formData.personalInputs.self_firstName}
+            </p>
+            <p>
+              <strong>Tên:</strong> {formData.personalInputs.self_lastName}
+            </p>
+            <p>
+              <strong>Tuổi:</strong> {formData.personalInputs.self_age}
+            </p>
+            <p>
+              <strong>CCCD:</strong> {formData.personalInputs.self_cccd}
+            </p>
+            <p>
+              <strong>Dân Tộc:</strong> {formData.personalInputs.self_danToc}
+            </p>
+            <p>
+              <strong>Tôn Giáo:</strong> {formData.personalInputs.self_tonGiao}
+            </p>
+            <p>
+              <strong>Email:</strong> {formData.personalInputs.self_email}
+            </p>
+            <p>
+              <strong>Ngày Sinh:</strong> {formData.selfDateOfBirth}
+            </p>
+            <p>
+              <strong>Địa Chỉ Cá Nhân:</strong> {formData.personalAddress.tinh},{" "}
+              {formData.personalAddress.huyen}, {formData.personalAddress.xa}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,104 +1,307 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const MainForm = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [address, setAddress] = useState([]);
-  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const address = [
+    {
+      id: 1,
+      name: "tinh",
+      value: [
+        {
+          id: "01",
+          name: "Hà Nội",
+        },
+        {
+          id: "02",
+          name: "Đà Nẵng",
+        },
+        {
+          id: "03",
+          name: "Hồ Chí Minh",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "huyen",
+      value: [
+        {
+          id: "01",
+          name: "Quận 1",
+        },
+        {
+          id: "02",
+          name: "Quận 2",
+        },
+        {
+          id: "03",
+          name: "Quận 3",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "xa",
+      value: [
+        {
+          id: "01",
+          name: "Phường 1",
+        },
+        {
+          id: "02",
+          name: "Phường 2",
+        },
+        {
+          id: "03",
+          name: "Phường 3",
+        },
+      ],
+    },
+  ];
 
-  // Define options for address components
-  const [districtOptions, setDistrictOptions] = useState([
-    "Tỉnh 1",
-    "Tỉnh 2",
-    "Tỉnh 3",
-  ]);
-  const [villageOptions, setVillageOptions] = useState([
-    "Huyện 1",
-    "Huyện 2",
-    "Huyên 3",
-  ]);
-  const [townOptions, setTownOptions] = useState(["Xã 1", "Xã 2", "Xã 3"]);
+  // personal information
+  const [personalInputs, setPersonalInputs] = useState({
+    self_firstName: "",
+    self_lastName: "",
+    self_age: "",
+    self_cccd: "",
+    self_danToc: "",
+    self_tonGiao: "",
+    self_email: "",
+  });
+  const [selfDateOfBirth, setSelfDateOfBirth] = useState("");
+  const [selfPicture, setSelfPicture] = useState(null);
+  const [selfPicturePreview, setSelfPicturePreview] = useState(null);
+  const [selfImagePictureUrl, setSelfImagePictureUrl] = useState("");
+  const [personalAddress, setPersonalAddress] = useState({
+    tinh: address[0].value[0].name,
+    huyen: address[1].value[0].name,
+    xa: address[2].value[0].name,
+  });
 
-  const [district, setDistrict] = useState("");
-  const [village, setVillage] = useState("");
-  const [town, setTown] = useState("");
+  // todo: parent information
+
+  const handlerOnChangePersonalInput = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setPersonalInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handlerSelectPicture = (event) => {
+    const file = event.target.files[0];
+    setSelfPicture(file);
+    const imageUrl = URL.createObjectURL(file);
+    setSelfImagePictureUrl(imageUrl);
+
+    // Read the file as a data URL
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      // Set the data URL as the picture source
+      setSelfPicturePreview(e.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    const data = {
+      personalInputs,
+      personalAddress,
+      selfDateOfBirth,
+      selfPicture,
+      selfImagePictureUrl,
+    };
+    console.log(data);
+    navigate("/", { state: { data } });
+
+    // console.log(data);
+  };
+
   return (
-    <form className="personal-info-form">
-      <label>
-        Họ:
-        <input
-          type="text"
-          name="name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </label>
-      <label>
-        Tên:
-        <input
-          type="text"
-          name="name"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </label>
+    <form className="personal-info-form" onSubmit={handleSubmitForm}>
+      <div>
+        <h4>Thông tin cá nhân</h4>
+        <label>
+          Họ:
+          <input
+            type="text"
+            name="self_firstName"
+            value={personalInputs.self_firstName}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
+        <label>
+          Tên:
+          <input
+            type="text"
+            name="self_lastName"
+            value={personalInputs.self_lastName}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
 
-      <label>
-        Tuổi:
-        <input
-          type="number"
-          name="age"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </label>
-      <label>
-        Địa chỉ: <br />
-        <select onChange={(e) => setDistrict(e.target.value)}>
-          <option value="">Chọn tỉnh</option>
-          {districtOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
+        <label>
+          Tuổi:
+          <input
+            type="number"
+            name="self_age"
+            value={personalInputs.self_age}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
+        <label>
+          CCCD:
+          <input
+            type="text"
+            name="self_cccd"
+            value={personalInputs.self_cccd}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
+        <label>
+          Dân Tộc:
+          <input
+            type="text"
+            name="self_danToc"
+            value={personalInputs.self_danToc}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
+        <label>
+          Tôn giáo:
+          <input
+            type="text"
+            name="self_tonGiao"
+            value={personalInputs.self_tonGiao}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
+
+        <label>
+          Email:
+          <input
+            type="email"
+            name="self_email"
+            value={personalInputs.self_email}
+            onChange={handlerOnChangePersonalInput}
+          />
+        </label>
+        <label>
+          Ngày Sinh:
+          <input
+            type="date"
+            name="selfDateOfBirth"
+            value={selfDateOfBirth}
+            onChange={(e) => setSelfDateOfBirth(e.target.value)}
+          />
+        </label>
+        <label className="picture">
+          Ảnh
+          <input type="file" onChange={handlerSelectPicture} />
+        </label>
+        {selfPicturePreview && (
+          <img
+            src={selfPicturePreview}
+            alt="Preview"
+            style={{ maxWidth: "400px", marginTop: "10px" }}
+          />
+        )}
+        <label>Địa Chỉ Cá nhân</label>
+
+        {/* Tinh (Province) Dropdown */}
+        <select
+          value={personalAddress.tinh}
+          onChange={(e) =>
+            setPersonalAddress({ ...personalAddress, tinh: e.target.value })
+          }
+        >
+          {address[0].value.map((tinh) => (
+            <option key={tinh.id} value={tinh.name}>
+              {tinh.name}
             </option>
           ))}
         </select>
-        <select onChange={(e) => setVillage(e.target.value)}>
-          <option value="">Chọn Huyện</option>
-          {villageOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
+
+        {/* Huyen (District) Dropdown */}
+        <select
+          value={personalAddress.huyen}
+          onChange={(e) =>
+            setPersonalAddress({
+              ...personalAddress,
+              huyen: e.target.value,
+            })
+          }
+        >
+          {address[1].value.map((item) => (
+            <option key={item.id} value={item.name}>
+              {item.name}
             </option>
           ))}
         </select>
-        <select onChange={(e) => setTown(e.target.value)}>
-          <option value="">Chọn Xã/Phường</option>
-          {townOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
+
+        {/* Xa (Ward) Dropdown */}
+        <select
+          value={personalAddress.xa}
+          onChange={(e) =>
+            setPersonalAddress({ ...personalAddress, xa: e.target.value })
+          }
+        >
+          {address[2].value.map((item) => (
+            <option key={item.id} value={item.name}>
+              {item.name}
             </option>
           ))}
         </select>
-      </label>
+      </div>
 
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
+      <div>
+        <h4>Thông tin người thân</h4>
+        <h5>Thông tin Cha</h5>
+        <label>
+          Nhập tên cha
+          <input type="text" />
+        </label>
+        <label>
+          Nhập tuổi cha
+          <input type="number" />
+        </label>
+        <label>
+          Nhập CCCD cha
+          <input type="text" />
+        </label>
+        <label>
+          Nhập địa chỉ
+          <input type="text" />
+        </label>
 
-      <button type="submit">Submit</button>
-      <p>{firstName}</p>
+        <h5>Thông tin mẹ</h5>
+        <label>
+          Nhập tên mẹ
+          <input type="text" />
+        </label>
+        <label>
+          Nhập tuổi mẹ
+          <input type="number" />
+        </label>
+        <label>
+          Nhập CCCD mẹ
+          <input type="text" />
+        </label>
+        <label>
+          Nhập địa chỉ
+          <input type="text" />
+        </label>
+      </div>
+
+      <input type="submit" />
+      {/*<p>{firstName}</p>
       <p>{lastName}</p>
       <p>{age}</p>
       <p>{district}</p>
       <p>{village}</p>
       <p>{town}</p>
-      <p>{email}</p>
+      <p>{email}</p> */}
     </form>
   );
 };
